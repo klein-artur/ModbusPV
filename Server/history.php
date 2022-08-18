937e9688acf741518df4ea6ac78841430e9dffa9
Artur Hellmann <artur@outbankapp.com>
2022-08-18 04:51:32 +0200

    Adding errors to history

--- Server/history.php
+++ Server/history.php
@@ -1,5 +1,9 @@
 <?php
-    header('Content-type: application/json');\
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    header('Content-type: application/json');
 
     include 'db/sqliteConnect.php';
 
