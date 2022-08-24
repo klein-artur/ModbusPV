f8d37b07fea441bc5435e303ec3f54d759a4b1c1
Artur Hellmann <artur@outbankapp.com>
2022-08-24 14:40:51 +0200

    again changed

--- Server/db/sqliteConnect.py
+++ Server/db/sqliteConnect.py
@@ -78,7 +78,7 @@ def insertReading(reading, forecasts):
             forecastUpdateSql = "insert or replace into forecasts (timestamp, forecast) values (?, ?);"
             cur.execute(forecastUpdateSql, [timestamp, forecast])
 
-            if timestamp < time() - 86400 and forecast > 0:
+            if timestamp < time() - 21600 and forecast > 0:
                 factorUpdateSQL = '''      
                                     UPDATE
                                         forecastFactor
