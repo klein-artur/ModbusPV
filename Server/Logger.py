085db2a1153b19168332b7bf8834c4c9ba631a51
Artur Hellmann <artur.hellmann@yahoo.de>
2022-09-14 15:58:22 +0200

    prepend logs instead of appending

--- Server/Logger.py
+++ Server/Logger.py
@@ -1,8 +1,14 @@
 from datetime import datetime
 
 def log(message=None):
-    with open("logfile.log", "a+") as logfile:
+    current = ''
+    with open("logfile.log", 'r+') as logfile:
+        current = logfile.read()
+        logfile.close()
+
+    with open("logfile.log", "w+") as logfile:
         if message is not None:
-            logfile.write(f'{datetime.now()} -- {message}\n')
+            logfile.write(f'{datetime.now()} -- {message}\n{current}')
         else:
-            logfile.write('\n\n')
+            logfile.write(f'\n\n{current}')
+        logfile.close()
