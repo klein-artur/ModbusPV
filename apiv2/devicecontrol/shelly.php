a355254356c9ecd14de40b7c18cccf9066bf2244
Artur Hellmann <artur.hellmann@yahoo.de>
2022-11-08 18:12:27 +0100

    A try to retry shelly calls if timed out.

--- apiv2/devicecontrol/shelly.php
+++ apiv2/devicecontrol/shelly.php
@@ -1,40 +1,57 @@
 <?php
 
-    function switchDevice($device, bool $on) {
-        $curl = curl_init();
-
-        $url = $device['api_url'].'/device/relay/control';
-
-        $postfields = $device['parameter'];
-        $postfields['turn'] = $on ? 'on' : 'off';
-
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => $postfields
-        ));
-        
-        $response = curl_exec($curl);
+    function switchDevice($device, bool $on): bool {
 
         $isSuccess = false;
-        if ($response === false) 
-            echo curl_error($curl);
-        else {
-            $result = json_decode($response);
+        $tries = 0;
+        $shouldTry = true;
+
+        while ($shouldTry && $tries != 4) {
+            $tries++;
+            $shouldTry = false;
+            $curl = curl_init();
+
+            $url = $device['api_url'].'/device/relay/control';
+
+            $postfields = $device['parameter'];
+            $postfields['turn'] = $on ? 'on' : 'off';
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => $postfields
+            ));
             
-            $isSuccess = $result->isok;
-            if (!$isSuccess) {
-                print_r ($result);
+            $response = curl_exec($curl);
+            if ($response === false) 
+                echo curl_error($curl);
+            else {
+                $result = json_decode($response);
+                
+                $isSuccess = $result->isok;
+                if (!$isSuccess) {
+                    if (property_exists($result, 'errors')) {
+                        $errors = $result->errors;
+                        if (property_exists($errors, 'max_req')) {
+                            $shouldTry = true;
+                            sleep(1);
+                        } else {
+                            print_r($result);
+                        }
+                    } else {
+                        print_r($result);
+                    }
+                }
             }
+            
+            curl_close($curl);
         }
-        
-        curl_close($curl);
         return $isSuccess;
     }
 ?>
