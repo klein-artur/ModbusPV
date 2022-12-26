ef712e498ec63bee75a6f28034d6a84f343c6a97
Artur Hellmann <artur.hellmann@yahoo.de>
2022-12-26 21:32:42 +0100

    Added change device api for name and estimated consumption

---
+++ apiv2/changeDevice.php
@@ -0,0 +1,94 @@
+<?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    header('Content-type: application/json');
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $deviceIdentifier = $_POST['identifier'];
+
+        $string = file_get_contents("../deviceconfig.json");
+        $config = json_decode($string,true);
+
+        $found;
+
+        foreach ($config as &$device) {
+            if (isset($device['priority'])) {
+                if ($device['identifier'] == $deviceIdentifier) {
+                    $found = $device;
+                }
+            }
+        }
+
+        if (!$found) {
+            echo json_encode([
+                'result' => false,
+                'output' => "device identifier not valid."
+            ]);
+            exit;
+        }
+
+        if (isset($_GET['prio'])) {
+
+            $prio = $_POST['prio'];
+
+            $samePrio;
+            
+            if ($device['priority'] == $prio) {
+                $samePrio = $found;
+            }
+
+            if ($prio < 0 || $prio > 100) {
+                echo json_encode([
+                    'result' => false,
+                    'output' => "value not valid."
+                ]);
+                exit;
+            }
+    
+            if (isset($samePrio) && $found !== $samePrio) {
+                echo json_encode([
+                    'result' => false,
+                    'output' => "A device with the same priority already exists."
+                ]);
+                exit;
+            }
+    
+            $found['priority'] = intval($prio);
+
+        }
+
+        if (isset($_GET['name'])) {
+            $found['name'] = $_POST['name'];
+        }
+
+        if (isset($_GET['estimated_consumption'])) {
+            $found['estimated_consumption'] = intval($_POST['estimated_consumption']);
+        }
+
+        $result = [];
+        foreach ($config as $device) {
+            if ($device['identifier'] == $deviceIdentifier) {
+                $result[] = $found;
+            } else {
+                $result[] = $device;
+            }
+        }
+
+        $newContent = json_encode($result, JSON_PRETTY_PRINT);
+
+        file_put_contents("../deviceconfig.json", $newContent);
+
+        echo json_encode([
+            'result' => true,
+            'output' => 'done'
+        ]);
+    } else {
+        echo json_encode([
+            'result' => false,
+            'output' => 'Only POST method is allowed.'
+        ]);
+    }
+    
+?>
